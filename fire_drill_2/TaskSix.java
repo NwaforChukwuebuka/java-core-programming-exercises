245ebe19e363710691156b995093ff89c6892b82
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-17 16:01:59 +0100

    Print Average of task 5

---
+++ fire_drill_2/TaskSix.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TaskSix{
+
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+	int sum = 0;
+	int count = 0;
+	int avg = 0;
+	for (int i = 0; i++ < 10;) {
+		System.out.printf("Enter Score: ");
+		float num = sc.nextFloat();
+		if (num % 2 == 0) {
+		count++;
+		sum += num;
+		}
+	}
+	avg = sum/count;
+	System.out.printf("Avg: %d", avg);
+    }
+  }
