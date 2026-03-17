d9f16d48760ce21318343c229675a7ecc48a5232
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-17 15:59:52 +0100

    Sum and Average of 10 scores

---
+++ fire_drill_2/TaskThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TaskThree{
+
+	public static void main(String[] args){
+	float average;
+	Scanner sc = new Scanner(System.in);
+	float sum = 0;
+	for (int i = 0; i++ < 10;) {
+		System.out.printf("Enter Score: ");
+		Float num = sc.nextFloat();
+		sum += num;
+	}
+	System.out.printf("Total Score: %f\n", sum);
+	System.out.printf("Average Score: %f", sum/10);
+    }
+  }
