8a39bbd33054e83b69a6d2ffbb5adb13f25d87b9
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-17 16:01:29 +0100

    Collects 10 scores and print only the even scores

---
+++ fire_drill_2/TaskFive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaskFive{
+
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+	int sum = 0;
+	for (int i = 0; i++ < 10;) {
+		System.out.printf("Enter Score: ");
+		float num = sc.nextFloat();
+		if (num % 2 == 0) {
+		sum += num;
+		}
+	}
+	System.out.printf("Total Score: %d", sum);
+    }
+  }
