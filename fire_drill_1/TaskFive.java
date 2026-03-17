93fff30bf3440d6a049bb84c53264ec33a98fa4b
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-17 12:59:20 +0100

    Prints each result from TaskFour , % times each

---
+++ fire_drill_1/TaskFive.java
@@ -0,0 +1,17 @@
+public class TaskFive{
+
+	public static void main(String[] args){
+
+	
+	for (int i = 0; i++ < 10;) {
+	if (i % 4 == 0) {
+		int j = 1;
+		while (j < 5) {
+		System.out.printf("%d",i);
+		j++;
+		}
+		System.out.printf(" ");
+	}
+    }
+  }
+}
