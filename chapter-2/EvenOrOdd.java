683e0374c2f5b55dad721aad8539e0d1442976d5
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-17 09:19:09 +0100

    Ignore Java class files
    Adds .gitignore to ignore *.class so compiled Java bytecode files do not appear as untracked.

--- chapter-2/EvenOrOdd.java
+++ chapter-2/EvenOrOdd.java
@@ -9,10 +9,13 @@ public class EvenOrOdd {
 		int number = scanner.nextInt();
 		scanner.close();
 
-		if (number % 2 == 1)
-			System.out.printf("%d is odd", number);
-		if (number % 2 == 0)
-			System.out.printf("%d is even", number);
+		if (number % 3  == 0)
+			System.out.printf("%d is divisible by 3", number);
+		if (number % 3 == 1)
+			System.out.printf("%d is not divisible by 3", number);
+		if (number % 3 == 2)
+			System.out.printf("%d is not divisible by 3", number);
+
 
 
 	}
