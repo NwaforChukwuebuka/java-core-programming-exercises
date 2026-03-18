e6fb0e3ada050203a329e95157d3a27566181959
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-18 03:54:00 +0100

    Added checks for numbers greater or equal to zero

--- chapter-2/DiameterCircumferenceAndArea.java
+++ chapter-2/DiameterCircumferenceAndArea.java
@@ -8,10 +8,14 @@ public class DiameterCircumferenceAndArea {
 		System.out.print("Enter the Radius of the Circle: ");
 		int radius = sc.nextInt();
 		sc.close();
-
+		if (radius > 0) {
 		System.out.printf("Diameter =  %d%n", radius * 2);
 		System.out.printf("Circumference =  %f%n", 2 * Math.PI * radius);
 		System.out.printf("Area = %f%n", Math.PI * (radius * radius));
+		} 
+		if (radius <= 0) {
+		System.out.println("Radius must be greater than 0");
+		}
 	}
 
 }
