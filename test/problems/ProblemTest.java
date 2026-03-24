289557372913422fd495e41995d059688eae4d01
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-24 04:46:54 +0100

    test: verify new Problem is assigned correct type

--- test/problems/ProblemTest.java
+++ test/problems/ProblemTest.java
@@ -10,8 +10,23 @@ public class ProblemTest {
     @Test
     @DisplayName("Every new problem should have a name")
     public void newProblemShouldHaveName(){
-        Problem newProblem = new Problem("School Fees Debit", ProblemType.FINANCIAL);
+        Problem newProblem = new Problem("School Fees Debit", ProblemType.FINANCIAL, false);
         assertEquals("School Fees Debit", newProblem.getName());
-
     }
+
+    @Test
+    @DisplayName("Every new problem should have type")
+    public void newProblemShouldHaveType(){
+        Problem newProblem = new Problem("School Fees Debit", ProblemType.FINANCIAL, false);
+        assertEquals(ProblemType.FINANCIAL, newProblem.getType());
+    }
+
+
+    @Test
+    @DisplayName("Every new problem should not be solved")
+    public void newProblemShouldNotBeSolved(){
+        Problem newProblem = new Problem("School Fees Debit", ProblemType.FINANCIAL, false);
+        assertEquals(false,newProblem.isSolved());
+    }
+
 }
