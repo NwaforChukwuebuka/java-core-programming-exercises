de4974e29491a0c9dce68ae5ac26d975f3e11a9b
Chukwuebuka Samuel Nwafor <145198300+NwaforChukwuebuka@users.noreply.github.com>
2026-03-24 04:48:29 +0100

    feat(problem): add getters for type and solved status
    - Implement getType() to return problem type
    - Implement isSolved() to return problem solved state

--- src/problems/Problem.java
+++ src/problems/Problem.java
@@ -2,12 +2,25 @@ package problems;
 
 public class Problem {
     private String name;
+    private ProblemType type;
+    private boolean isSolved = false;
 
-    public Problem(String name, ProblemType type) {
+    public Problem(String name, ProblemType type, boolean isSolved) {
         this.name = name;
+        this.type = type;
+        this.isSolved = isSolved;
+
     }
 
     public String getName() {
         return  name;
+    }
+
+    public ProblemType getType() {
+        return type;
+    }
+
+    public boolean isSolved() {
+        return isSolved;
     }
 }
